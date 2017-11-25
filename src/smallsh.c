5cc4bde56e6b72d9dbc5993e2dcde23c3db20add
SsangWoo <tkddn204@gmail.com>
2017-11-25 19:12:35 +0900

    민수의 smallsh.c

--- src/smallsh.c
+++ src/smallsh.c
@@ -1,3 +1,26 @@
+#include "smallsh.h"
+
+// common constant values
+#define EOL 1 /* End Of Line */
+#define ARG 2 /* normal argument */
+#define AMPERSAND 3
+#define SEMICOLON 4
+#define MAXARG 512 /* max. no. command args */
+#define MAXBUF 512 /* max. length input line */
+#define FOREGROUND 0
+#define BACKGROUND 1
+
+// new constant value
+#define PIPE 5
+#define REDIRECTION_LEFT 6
+#define REDIRECTION_RIGHT 7
+
+static char inpbuf[MAXBUF], tokbuf[2*MAXBUF], *ptr, *tok;
+static char special[] = {' ', '\t', '&', ';', '\n', '\0', '|', '<', '>'};
+
+/* 프롬프트(prompt)를 인쇄하고 키보드에서 한 줄의 */
+/* 입력이 들어오기를 기다린다. 받은 입력은 무엇이건 */
+/* 프로그램 버퍼에 저장한다. */
 int userin(char *p) {
     FILE *fp;
     char *home = getenv("HOME");
@@ -43,4 +66,132 @@ int userin(char *p) {
     fputs(inpbuf, fp);
     fclose(fp);
     return count;
+}
+/* get token and place into tokbuf */
+int gettok(char **outptr)
+{
+    int type;
+    /* outptr 문자열을 tok 로 지정한다 */
+    *outptr = tok;
+    /* 토큰을 포함하고 있는 버퍼로부터 여백을 제거한다 */
+    while( *ptr == ' ' || *ptr == '\t') ptr++;
+    /* 토큰 포인터를 버퍼 내의 첫 토큰으로 지정한다 */
+    *tok++ = *ptr;
+    /* 버퍼내의 토큰에 따라 유형 변수를 지정한다 */
+    switch(*ptr++) {
+        case '\n' : type = EOL;
+        /* printf(" type == EOL getok()\n"); */
+        break;
+        case '&' : type = AMPERSAND;
+        /* printf(" type == AMPERSAND getok()\n"); */
+        break;
+        case ';' : type = SEMICOLON;
+        /* printf(" type == SEMICOLON getok()\n"); */
+        break;
+        case '|' : type = PIPE;
+        /* printf(" type == PIPE getok()\n"); */
+        break;
+        case '<' : type = REDIRECTION_LEFT;
+        /* printf(" type == REDIRECTION_LEFT getok()\n"); */
+        break;
+        case '>' : type = REDIRECTION_RIGHT;
+        /* printf(" type == REDIRECTION_RIGHT getok()\n"); */
+        break;
+        default : type = ARG;
+        /* printf(" type == ARG getok()\n"); */
+        while(inarg(*ptr)) *tok++ = *ptr++;
+        break;
+    }
+    *tok++ = '\0';
+    return type;
+}
+
+/* are we in an ordinary argument */
+int inarg(char c)
+{
+    char *wrk;
+    for(wrk = special; *wrk != '\0'; wrk++) {
+        if (c == *wrk) {
+            //printf(" special arg : %c inarg()\n", *wrk);
+            return 0;
+        }
+    }
+    return 1;
+}
+
+/* 입력 줄을 아래와 같이 처리한다 : */
+/* */
+/* gettok을 이용하여 명령문을 구무분석(parse) 하고 */
+/* 그 과정에서 인수의 목록을 작성한다. 개행문자나 */
+/* 세미콜론(;)을 만나면 명령을 수행하기 위해 */
+/* runcommand라 불리는 루틴을 호출한다. */
+void procline()
+{
+    char *arg[MAXARG+1]; /* runcommand를 위한 포인터 배열 */
+    int toktype; /* 명령내의 토근의 유형 */
+    int narg; /* 지금까지의 인수 수 */
+    int type; /* FOREGROUND or BACKGROUND */
+    /* 토큰 유형에 따라 행동을 취한다. */
+    for (narg = 0;;) { /* loop FOREVER */
+        switch(toktype = gettok(&arg[narg])) {
+            case ARG : if (narg < MAXARG) narg++;
+            break;
+            case EOL :
+            case SEMICOLON :
+            case AMPERSAND :
+            type = (toktype == AMPERSAND) ?
+                BACKGROUND : FOREGROUND;
+            if (narg != 0) {
+                arg[narg] = NULL;
+                runcommand(narg, arg, type);
+            }
+            if (toktype == EOL) return;
+            narg = 0;
+            break;
+            // case PIPE :
+            // TODO: 파이프
+            // case REDIRECTION_LEFT :
+            // case REDIRECTION_RIGHT :
+            // TODO: 리다이렉션
+            // break;
+        }
+    }
+}
+/* wait를 선택사항으로 하여 명령을 수행한다. */
+/* 명령을 수행하기 위한 모든 프로세스를 시작하게 한다. */
+/* 만일 where가 smallsh.j에서 정의된 값 BACKGROUND로 */
+/* 지정되어 있으면 waitpid 호출은 생략되고 runcommand는 */
+/* 단순히 프로세스 식별번호만 인쇄하고 복귀한다. */
+int runcommand(int argc, char **cline, char where)
+{
+    int pid; // , exitstat, ret;
+    int status;
+    int pr_code;
+
+    if ((pid = fork()) < 0) {
+        perror("smallsh");
+        return -1;
+    }
+    
+    pr_code = command_parser(pid, argc, cline);
+    if (pid == 0) { /* child */
+        if(pr_code == -1) {
+            execvp(*cline, cline);
+            perror(*cline);
+            exit(127);
+        } else {
+            exit(0);
+        }
+    }
+    /* code for parent */
+    /* if background process, print pid and exit */
+    if (where == BACKGROUND) {
+        printf("[Process id %d]\n",pid);
+        return 0;
+    }
+    /* 프로세스 pid가 퇴장할 때까지 기다린다. */
+    if (waitpid(pid, &status, 0) == -1)
+        return -1;
+    else
+    return status;
 }
