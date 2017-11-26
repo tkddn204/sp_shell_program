6cb3f998fad6398358bfe3089b892550859b2b1a
SsangWoo <tkddn204@gmail.com>
2017-11-27 00:39:47 +0900

    asdf

--- src/project_pipe_redirection.c
+++ src/project_pipe_redirection.c
@@ -2,57 +2,63 @@
 
 int runcommand_pipe(int argc, char **cline, char where)
 {
-    int left_fd[2], right_fd[2];
-    char buf[BUF_SIZE];
-    int pid; // , exitstat, ret;
+    int pid[2]; // , exitstat, ret;
     int status;
     int pr_code;
 
-    if (pipe(left_fd) < 0 || pipe(right_fd) < 0) {
-        perror("smallsh pipe error");
-        return -1;
-    }
+    int fd[2];
+    char buf[BUF_SIZE];
+    int i;
 
-    if ((pid = fork()) < 0) {
-        perror("smallsh error");
-        return -1;
-    }
-    
-    pr_code = command_parser(pid, argc, cline);
-    if (pid == -1) {
-        perror("failed fork");
-        exit(1);
-    } else if (pid == 0) { /* child */
-        if(pr_code == -1) {
-            execvp(*cline, cline);
-            perror(*cline);
-            exit(127);
-        } else {
-            exit(0);
+    for(i = 0; i < 2; i++) {
+
+        if (pipe(fd) < 0) {
+            perror("smallsh pipe error");
+            return -1;
         }
-    } else if (pid > 0) {
-        while(1) {
-            write(stdout, buf, strlen( buf));
-            memset( buf, 0, BUF_SIZE);
-            read( left_fd[0], buf, BUF_SIZE);
-            printf( "부모 프로세스: %s\n", buf);
-            if(buf == NULL) {
-                break;
+
+        if ((pid[i] = fork()) < 0) {
+            perror("smallsh error");
+            return -1;
+        }
+
+        if (pid[i] == -1) {
+            perror("failed fork");
+            exit(1);
+        } else if (pid[i] == 0) { /* child */
+            if(i == 0) {
+                dup2(fd[1], 1);
+            } else if(i > 0) {
+                dup2(fd[0], 0);
             }
+            pr_code = command_parser(pid, argc, cline);
+            if(pr_code == -1) {
+                execvp(*cline, cline);
+                perror(*cline);
+                exit(127);
+            } else {
+                exit(0);
+            }
+        } else if (pid[i] > 0) {
+            dup2(fd[1], 1);
+            pr_code = command_parser(pid, argc, cline);
         }
-    }
-    /* code for parent */
-    /* if background process, print pid and exit */
-    if (where == BACKGROUND) {
-        printf("[Process id %d]\n",pid);
-        return 0;
-    }
 
-    /* 프로세스 pid가 퇴장할 때까지 기다린다. */
-    if (waitpid(pid, &status, 0) == -1)
-        return -1;
-    else
-        return status;
+        /* code for parent */
+        /* if background process, print pid and exit */
+        if (where == BACKGROUND) {
+            printf("[Process id %d]\n",pid);
+            return 0;
+        }
+
+        /* 프로세스 pid가 퇴장할 때까지 기다린다. */
+        if (waitpid(pid[i], &status, 0) == -1)
+            return -1;
+        else
+            return status;
+    }
+    close(fd[0]);
+    close(fd[1]);
 }
 
 int runcommand_redirection(int argc, char **cline, char where)
